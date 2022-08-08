ea3fbe2723e47b295c28d11ace2b21958ffc29fc
Ирина <@DESKTOP-MSGGBKL>
2022-08-09 00:17:29 +0500

    Добавьте файлы проекта.

---
+++ lab_1.2/lab_1.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float n, m, a, b, c;
+	cin >> n;
+	cin >> m;
+	a = ++n * ++m; // (++n)перед исполнением кода увелич знач на единицу 
+	cout << a << endl;
+
+	cin >> n;
+	cin >> m;
+	b = m++ < n; // (m++)после исполнением кода увелич знач на единицу
+	cout << b << endl;
+
+	cin >> n;
+	cin >> m;
+	c = n++ > m;
+	cout << c << endl;
+
+}
