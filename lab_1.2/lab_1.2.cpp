668f461bf5bd3213e9c6002de029d2795631aa94
Irissha <110852608+Irissha@users.noreply.github.com>
2022-09-12 19:26:06 +0500

    Update lab_1.2.cpp

--- lab_1.2/lab_1.2.cpp
+++ lab_1.2/lab_1.2.cpp
@@ -1,20 +1,16 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
+int main() {
 	float n, m, a, b, c;
 	cin >> n;
 	cin >> m;
-	a = ++n * ++m; // (++n)перед исполнением кода увелич знач на единицу 
-	cout << a << endl;
-
-	cin >> n;
-	cin >> m;
-	b = m++ < n; // (m++)после исполнением кода увелич знач на единицу
-	cout << b << endl;
-
-	cin >> n;
-	cin >> m;
+	a = ++n * ++m; // (++)перед исполнением кода увелич знач на единицу 
+	cout << "\n1)" << a;
+	cout << "\n";
+	b = m++ < n;  =
+	cout << "\n2)" << b;
+	cout << "\n";
 	c = n++ > m;
-	cout << c << endl;
+	cout << "\n3)" << c;
+	cout << "\n";
 }
