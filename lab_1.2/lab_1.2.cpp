104927f0d33dd4d245a2ebe081f74d687ab4a42a
Irissha <110852608+Irissha@users.noreply.github.com>
2022-09-22 23:22:19 +0500

    Update lab_1.2.cpp

--- lab_1.2/lab_1.2.cpp
+++ lab_1.2/lab_1.2.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 int main() {
 	float n, m, a, b, c;
-	
+	 
+	setlocale(LC_ALL, "rus");
+
+	cout << "Введите два числа: ";
 	cin >> n;
 	cin >> m;
-	a = ++n * ++m; 
-	
-	cout << "\n1)" << a << "\n";
-	
-	b = m++ < n;  
-	cout << "\n2)" << b << "\n";
-	
+	a = ++n * ++m;
+
+	cout << " ++n * ++m => " << a << "\n";
+
+	b = m++ < n;
+	cout << " m++ < n => " << b << "\n";
+
 	c = n++ > m;
-	cout << "\n3)" << c << "\n";
+	cout << " n++ > m => " << c << "\n";
 }
